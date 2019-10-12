1c8bcebbe202b62b2054447d8911144078304574
dms449 <stevensondm23@gmail.com>
2019-10-11 22:46:32 -0500

    Can easily load and plot fft of part of song

--- first.jl
+++ first.jl
@@ -1,27 +1,11 @@
-using WAV
-using FFTW
-using DSP
 using Plots
-plotlyjs()
+gr()
+# plotlyjs()
+
+include("data_sets.jl")
+include("functions.jl")
 
 # constants
-sec_to_process = 20
+seconds = 3
 
-function get_fft(data, sample_freq)
-  F = fftshift(fft(data))
-  freqs = fftshift(fftfreq(size(data)[1], sample_freq))
-  return freqs, F
-end
-
-data, fs, nbits, opt = wavread("/home/dms449/workspace/JuliaProjects/MusicalFxExtraction/test/HazySunshine.wav")
-
-samples = data[1:Int64(sec_to_process*fs)]
-
-p1 = plot(samples, title="Time Series");
-
-# get the frequency spectrum
-fft_x, fft_y = get_fft(samples, fs)
-p2 = plot(fft_x, fft_y, title="Frequency Spectrum");
-
-#plot(p1, p2, layout=(1,2))
 
