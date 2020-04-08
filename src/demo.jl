d1a76e3cf98f775feecf06f816f91ec9aa4531a0
dms449 <stevensondm23@gmail.com>
2020-04-07 22:34:29 -0500

    added times to xaxis on plots and improved interchangeability of models

--- src/demo.jl
+++ src/demo.jl
@@ -19,14 +19,22 @@ end
 
 function plot_song(model, songpath=raw"/home/dms449/Music/Josh Groban/Closer/05 When You Say You Love Me.mp3")
   song = MP3.load(songpath)
-  answers = process(song, model)
+  step_size = 2*fs
+  answers = process(song, model, step_size)
 
+  num_ticks = 11
+  tick_step = div(size(answers,1), num_ticks-1)
+  tick_positions = collect(range(0,step=tick_step, length=num_ticks))
+  tick_labels = Dates.format.(Time.(0, div.(tick_positions, 60), mod.(tick_positions,60)), "MM:SS")
+  
+  
   instrument_labels = reshape(instruments, (1,length(instruments)))
-  return plot(answers, ticks=[1:length(answers)], labels=instrument_labels, title="$(splitpath(songpath)[end])", size=(600,800))
+  #return plot(answers, ticks=[1:length(answers)], labels=instrument_labels, title="$(splitpath(songpath)[end])", size=(600,800))
+  return plot(answers, xticks=(tick_positions, tick_labels), labels=instrument_labels, title="$(splitpath(songpath)[end])", size=(600,800))
 end
 
-function process(data, model)
-  parts = partition(data[:,1], 2*fs, 2*fs)
+function process(data, model, step=2*fs)
+  parts = partition(data[:,1], 2*fs, step)
   spect = get_spectograms(collect(parts))
   answers = Array{Float32, 2}(undef, length(spect), length(instruments))
   for (i,each) in enumerate(spect)
--- src/model.jl
+++ src/model.jl
@@ -6,51 +6,47 @@ using Random
 include("data.jl")
 include("functions.jl")
 
-# data, labels = load_dir()
 
+instrument_model2 = Chain(
+    Conv((3, 3), 1=>16, pad=(1,1), relu),
+    MaxPool((2,2)),
 
-#samples = [stft(data[i,:],2750, 1375)[end] for i in 1:size(data)[1]]
-#function train_test(x, l)
-#  all = gpu.(shuffle!(collect(zip(x, l))))
-#  #test = gpu.(zip(x[end-3:end], l[end-3:end]))
-#  return all[1:end-2], all[end-1:end]
-#end
-#
-#function get_data()
-#  data, labels = load_dir()
-#  temp = get_spectograms(data)
-#  return train_test(temp, labels)
-#end
-#
-#function training(train, test)
-#  @epochs 1 Flux.train!(loss, params(c), train, opt, cb = throttle(evalcb, 5))
-#end
+    Conv((3, 3), 16=>32, pad=(1,1), relu),
+    MaxPool((2,2)),
 
-"""
+    Conv((3, 3), 32=>32, pad=(1,1), relu),
+    MaxPool((2,2)),
 
-"""
-function train(; epochs::Int=5)
-  # load the truth files
-  train_files,train_keys, test_files, test_keys = all_data()
-  test_samples = get_data(test_files, test_keys, 1, Int(2*44100), Int(2*44100))
+    x->reshape(x, :, size(x, 4)),
+   )
 
-  loss(x, y) = mse(instrument_model(x), y)
-  evalcb = () -> @show loss(test_samples[end][1], test_samples[end][2])
-  opt = ADAM()
-  
-  for i in 1:epochs
-    @info "epoch $i"
-    training_samples = get_data(train_files, train_keys, 5, Int(2*44100), Int(2*44100))
-    while !isempty(training_samples)
-      Flux.train!(loss, params(instrument_model), training_samples, opt, cb = throttle(evalcb, 1))
+instrument_model3 = Chain(
+    Conv((3, 3), 1=>8, pad=(1,1), relu),
+    MaxPool((2,3)),
 
-      training_samples = get_data(train_files, train_keys, 5, Int(2*44100), Int(2*44100))
-    end
-    
-    train_keys, test_keys = data_keys(train_files, test_files)
-  end
-end
+    Conv((3, 3), 8=>16, pad=(1,1), relu),
+    MaxPool((2,3)),
 
+    Conv((3, 3), 16=>16, pad=(1,1), relu),
+    MaxPool((2,3)),
+
+    x->reshape(x, :, size(x, 4)),
+
+    Dense(5472, length(instruments))
+   )
+
+instrument_model4 = Chain(
+    Conv((3, 3), 1=>16, pad=(1,1), relu),
+    MaxPool((2,3)),
+
+    Conv((3, 3), 16=>32, pad=(1,1), relu),
+    MaxPool((2,3)),
+
+    Conv((3, 3), 32=>32, pad=(1,1), relu),
+    MaxPool((2,3)),
+
+    x->reshape(x, :, size(x, 4)),
+   )
 
 
 instrument_model = Chain(
@@ -73,6 +69,29 @@ instrument_model = Chain(
    )
 
 
+""" 
+"""
+function train(model=instrument_model; epochs::Int=5)
+  # load the truth files
+  train_files,train_keys, test_files, test_keys = all_data()
+  test_samples = get_data(test_files, test_keys, 1, Int(2*44100), Int(2*44100))
+
+  loss(x, y) = mse(model(x), y)
+  evalcb = () -> @show loss(test_samples[end][1], test_samples[end][2])
+  opt = ADAM()
+  
+  for i in 1:epochs
+    @info "epoch $i"
+    training_samples = get_data(train_files, train_keys, 5, Int(2*44100), Int(2*44100))
+    while !isempty(training_samples)
+      Flux.train!(loss, params(model), training_samples, opt, cb = throttle(evalcb, 1))
+
+      training_samples = get_data(train_files, train_keys, 5, Int(2*44100), Int(2*44100))
+    end
+    
+    train_keys, test_keys = data_keys(train_files, test_files)
+  end
+end
 #loss(x, y) = mse(c(x), y)
 #evalcb = () -> @show loss(test[end][1], test[end][2])
 #opt = ADAM()
